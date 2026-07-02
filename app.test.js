ee987174bcaeaccb9744a804cfd5c44115e857f2
Matheo <matheo691200@gmail.com>
2026-07-02 21:48:25 +0200

    fix: tests complets -> couverture 100% (Quality Gate passe, pipeline verte)

--- app.test.js
+++ app.test.js
@@ -11,3 +11,27 @@ test('soustrait 5 - 2 = 3', () => {
 test('multiplie 4 * 3 = 12', () => {
   expect(calc.multiplie(4, 3)).toBe(12);
 });
+
+test('divise 10 / 2 = 5', () => {
+  expect(calc.divise(10, 2)).toBe(5);
+});
+
+test('divise par zero leve une erreur', () => {
+  expect(() => calc.divise(1, 0)).toThrow('Division par zero');
+});
+
+test('valeurAbsolue(-5) = 5', () => {
+  expect(calc.valeurAbsolue(-5)).toBe(5);
+});
+
+test('valeurAbsolue(5) = 5', () => {
+  expect(calc.valeurAbsolue(5)).toBe(5);
+});
+
+test('estPair(4) = true', () => {
+  expect(calc.estPair(4)).toBe(true);
+});
+
+test('estPair(3) = false', () => {
+  expect(calc.estPair(3)).toBe(false);
+});
